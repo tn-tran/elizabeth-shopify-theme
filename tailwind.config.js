f2b0806f4ec30dfead873c8a65e432e77a1c2751
Tien Tran <tientran@Tiens-MacBook-Pro.local>
2022-06-07 15:20:22 -0700

    Fix tailwindcss imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   content: [
-    './assets/*.liquid',
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/*.liquid',
-    './templates/**/*.liquid'
+    'assets/*.liquid',
+    'layout/*.liquid',
+    'sections/*.liquid',
+    'snippets/*.liquid',
+    'templates/*.liquid',
+    'templates/customers/*.liquid'
   ],
   theme: {
     extend: {},
